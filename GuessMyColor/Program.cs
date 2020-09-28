c8756e57a779544f8d4d291403185b65e2dc517c
Opilane TTHK <opilane@tthk.ee>
2020-09-28 14:51:46 +0300

    WhileArray

---
+++ GuessMyColor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GuessMyColor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int computerNumber = rnd.Next(1, 4);
+            bool rightGuess = false;
+
+            while (!rightGuess)
+            {
+                Console.WriteLine("What is my color? ");
+                string userInput = Console.ReadLine().ToLower();
+
+                if (computerNumber == 1 && userInput == "red")
+                {
+                    Console.WriteLine("Correct!");
+                    rightGuess = true;
+                }
+                else if (computerNumber == 2 && userInput == "green")
+                {
+                    Console.WriteLine("Correct!");
+                    rightGuess = true;
+                }
+                else if (computerNumber == 3 && userInput == "blue")
+                {
+                    Console.WriteLine("Correct!");
+                    rightGuess = true;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect!");
+                }
+            }
+        }
+    }
+}
